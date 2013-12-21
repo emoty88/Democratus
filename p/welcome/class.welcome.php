115bde3306f4559410f3d6957abf7c63c916b529
Caner Türkmen <emoty88@gmail.com>
2013-12-21 22:02:16 +0200

    Giriş sayfası yenilendi

--- p/welcome/class.welcome.php
+++ p/welcome/class.welcome.php
@@ -12,14 +12,16 @@
             $model->initTemplate('ala','welcome');
             $model->view = 'welcome';
 
-            $model->addScript(TEMPLATEURL."ala/js/modernizr-2.6.2.min.js", "modernizr-2.6.2.min.js", 1);
-            $model->addScript(TEMPLATEURL."ala/js/jquery-1.8.3.min.js", "jquery-1.8.3.min.js", 1);
-            $model->addScript(TEMPLATEURL."ala/js/jquery-ui-1.9.1.custom.min.js", "jquery-ui-1.9.1.custom.min.js", 1);
-            $model->addScript(TEMPLATEURL."ala/js/jquery.caroufredsel.js", "jquery.caroufredsel.js", 1);
-            $model->addScript(TEMPLATEURL."ala/js/bootstrap.min.js", "bootstrap.min.js", 1);
-           	// $model->addScript(TEMPLATEURL."ala/js/app.js", "app.js", 1);
-            $model->addScript(TEMPLATEURL."ala/js/jquery.tmpl.js", "jquery.tmpl.js", 1);
-            $model->addScript(TEMPLATEURL."ala/js/jquery-ui.js", "jquery-ui.js", 1);
+			$model->addScript("http://code.jquery.com/jquery-1.10.1.min.js","jquery-1.10.1.min.js",1);
+			$model->addScript(TEMPLATEURL.'ala/js/bootstrap.min.js','bootstrap.min.js',1);
+			$model->addScript(TEMPLATEURL.'ala/js/script.js','script.js',1);
+			$model->addScript(TEMPLATEURL.'ala/js/lib/jquery.mousewheel-3.0.6.pack.js','jquery.mousewheel-3.0.6.pack.js',1);
+			$model->addScript(TEMPLATEURL.'ala/js/fancybox/jquery.fancybox.js','jquery.fancybox.js',1);
+			$model->addScript("
+				$(document).ready(function() {
+					$('.fnc').fancybox();
+				});
+			");
 
             $model->addScript($model->pluginurl . 'welcome.js', 'welcome.js', 1);
             $model->addScript("paths=".json_encode($model->paths));
@@ -34,223 +36,182 @@
 				}
 			}
 			
-            if(isset($_GET['register']))
-                $model->addScript("$(document).ready(function()".'{'."$('#wellcome-register-button').click()});");	
+            ?>
+                <header id="header" class="site-header">
+			        <div class="container">
+			          <div class="row-fluid">
+			            <div class="span12">
+			              <h1><a class="site-title pull-left" href="/"> Democratus</a></h1>
+			              <div class="user text-right">
+			                <a class="user-login fnc" href="#login" >Giriş</a> | 
+			                <a class="user-regidter fnc" href="#register">Kayıt Ol</a>
+			              </div>
+			            </div><!-- /span12 -->
+			          </div><!-- /row-fluid -->
+			        </div><!-- /container -->
+			      
+			      <div class="container" style="padding-bottom: 40px;">
+			        <div class="row-fluid">
+			          <div class="span12">
+			          
+			            <div class="header-description">
+			              <h2 style="line-height: 85px;">Shape The World <br/> With Your Opinnion!</h2>
+			              <!--<a class="header-btn" href="#">Join now</a> -->
+			            </div><!-- /header-description -->
+			            
+			            <div class="header-features">
+			              <div class="row-fluid">
+			                <div class="span6 header-feature">
+			                  <h3><i class="icon-random"></i> &nbsp; Share Your Ideas.</h3>
+			                </div>
+			                <div class="span6 header-feature">
+			                  <h3> <i class="icon-group"></i> &nbsp; Rule Your Country.</h3>
+			                 </div>
+			              </div>
+			            </div><!-- /header-features -->
+			            
+			          </div><!-- /span12 -->
+			        </div><!-- /row-fluid -->
+			      </div><!-- /container -->
+			    </header>
+			    
+			
+			
+			    <section id="testimonials" class="testimonials">
+			      <div class="container">
+			      
+			        <div id="testimonials-carousel" class="carousel slide">
+			          <div class="carousel-inner">
+			             {{welcomeagenda}}
+			          </div>
+			          <a class="carousel-control left" href="#testimonials-carousel" data-slide="prev">&lsaquo;</a>
+			          <a class="carousel-control right" href="#testimonials-carousel" data-slide="next">&rsaquo;</a>
+			        </div><!-- /testimonials-carousel -->
+			        
+			      </div><!-- /container -->
+			    </section>   
+			    
+			    
+			    <footer id="footer" class="footer">
+			      <div class="container">
+			        <div class="row-fluid">
+			          <div class="span12">
+			            <h1 class="footer-title pull-left"><a href="/"> Democratus</a></h1>
+			            
+			            <p class="pull-right copyright">
+			              &copy Copyright 2013.
+			            </p>
+			          </div>
+			        </div>
+			      </div>
+			    </footer>
+				<div style="display:none;">
+					<div id="login" class="login-form" style="margin-left:0;">
+					
+						<h1>Giriş Yap</h1> 
+						<p id="welcomeLogin-error" class="welcomeError" style=""></p>
+						<ul class="social-btn"> 
+							<li class="fb" onclick="location.href='/oauth/facebook'; ">
+								<span>Connect With Facebook</span>
+							</li>
+							<li class="tw" onclick="location.href='/oauth/twitter'; ">
+								<span>Connect With Twitter</span>
+							</li>
+						</ul>
+						<form id="login-form" method="post" action="/welcome2">
+						
+						<ul class="login-List">
+							<li><input class="inputText" id="login-user" type="text" placeholder="User name - E Mail"/></li>
+							<li><input class="inputText" id="login-pass" type="Password" placeholder="Password"/></li>
+						</ul>
+						</form>
+						<div class="formFooter">
+							<div class="formFooterButtons">
+								<button id="login-action" type="submit" class="btn btn-success" style="float:right; padding: 10px 20px; margin-right:4px;">
+									<span  class="buttonText">Login</span>
+								</button>
+							</div>
+							<div class="bootom-Link">
+								<a href="#forgotPass" class="forgotPassword fnc">Forget my Password</a>
+								<br> 
+								<a class="fnc" href="#register">Register</a>
+							</div>
+						</div>
+					</form>
+					</div>
+					<div id="register" class="login-form" style="margin-left:0;">
+					<form id="register-form" >	
+						<h1>Register</h1> 
+						<p id="welcomeRegister-error" class="welcomeError" style=""></p>
+						<ul class="login-List">
+							<li><input id="register-name" class="inputText" type="text" placeholder="Name"/></li>
+							<li><input id="register-user" class="inputText" type="text" placeholder="User name"/></li>
+							<li><input id="register-email" class="inputText" type="text" placeholder="E Mail"/></li>
+							
+							<li><input id="register-pass" class="inputText" type="password" placeholder="Password"/></li>
+							<li><input id="register-pass2" class="inputText" type="password" placeholder="Repassword"/></li>
+							<li>
+								<select id="register-sex" class="selectBox" >
+									<option value="unknow" selected="true">Sex</option>
+									<option value="male">Male</option>
+									<option value="female">Female</option>
+								</select>
+							</li>
+							<li>
+								<input type="checkbox" style="float: left; margin-left: 15px;" id="agree">
+								<p style="">I read the 
+									<a target="_blank" href="http://democratus.com/privacy" style="color:#db522f;font-family: Helvetica;">
+										terms of use 
+									</a>
+									,I accept it.
+								</p>
+							</li>
+						</ul>
+						<div class="formFooter">
+							<div class="formFooterButtons">
+								<button id="register-action" type="button" class="btn btn-success" style="float:right; padding: 10px 20px; margin-right:4px;">
+									<span class="buttonText">Register</span>
+								</button>
+							</div>
+							<div class="bootom-Link">
+								<a href="#forgotPass" class="forgotPassword fnc">Forget my password</a>
+								<br> 
+								<a class="fnc" href="#login">Login</a>
+							</div>
+						</div>
+					</form>
+
+					</div>	
+					<div id="forgotPass" class="login-form" style="margin-left:0;">
+						<h1>Forget my password</h1> 
+						<ul class="social-btn"> 
+							<li class="fb" onclick="location.href='/oauth/facebook'; ">
+								<span>Connect With Facebook</span>
+							</li>
+							<li class="tw" onclick="location.href='/oauth/twitter'; ">
+								<span>Connect With Twitter</span>
+							</li>
+						</ul>
+						<ul class="login-List">
+							<li><input class="inputText" type="text" placeholder="E Mail"/></li>
+							
+						</ul>
+						<div class="formFooter">
+							<div class="formFooterButtons">
+								<button type="submit" class="btn btn-success" style="float:right; padding: 10px 20px; margin-right:4px;">
+									<span class="buttonText">Change password</span>
+								</button>
+							</div>
+							<div class="bootom-Link">
+								<a href="#forgotPass" class="forgotPassword fnc">Forget my password</a>
+								<br> 
+								<a class="fnc" href="#register">Register</a>
+							</div>
+						</div>
+					</div>
+				</div>
+            <?php
 			
 		}
-        
-        public function main_old(){
-            global $model, $db;
-            
-            $model->title = 'The Political Network';
-            $model->description = 'by Which You Can Shape Your World';
-            
-            //print_r($_COOKIE);
-            $model->initTemplate('v2','welcome');
-            $model->view = 'wellcome';
-           
-            //$model->addScript(PLUGINURL . 'lib/en.js', 'en.js', 1 );
-            //$model->addScript(PLUGINURL . 'lib/'.$model->language.'.js', $model->language.'.js', 1);
-            $model->addScript(PLUGINURL . 'lib/combined.js', 'combined.js', 1 );
-            $model->addScript(PLUGINURL . 'lib/common.js', 'common.js', 1 );
-            $model->addScript($model->pluginurl . 'welcome.js', 'welcome.js', 1 );
-
-			$model->addScript(TEMPLATEURL . 'v2/static/js/custom-form-elements.js', 'custom-form-elements.js', 1 );
-            $model->addStyle(TEMPLATEURL . 'v2/static/css/bootstrap.css', 'bootstrap.css', 1 );
-            $model->addStyle(TEMPLATEURL . 'v2/static/css/bootstrap-responsive.css', 'bootstrap-responsive.css', 1 );
-            $model->addStyle($model->pluginurl  . 'welcome.css', 'welcome.css', 1 );
-            
-            $model->addStyle(PLUGINURL . 'lib/jquery-ui/jquery-ui.css', 'jquery-ui.css', 1 );
-            //$model->addStyle(TEMPLATEURL . 'default/form.css', 'jquery-ui.css', 1 );
-            
-        	if($model->paths[1]=="signin")
-			{
-				$model->addScript("$(document).ready(function(){formChange('formLogin'); });");
-			}
-            
-            //require_once(PLUGINPATH.'lib/recaptcha/recaptchalib.php'); 
-            
-?>
-<div id="changeButon" style="position: relative;    z-index: 10;">
-		<input type="button" id="girisBtn" class="girisBtn" value="Giriş Yap"  style="display:none;" onclick="formChange('form');" />
-		<input type="button" id="kaydolBtn" class="kaydolBtn" value="Kayıt Ol" onclick="formChange('formLogin');" />
-	</div>
-	<div id="welcomeCenter" >
-		<img src="<?=TEMPLATEURL?>/v2/static/images/welcomeCenter.png" />
-	</div>
-	<div id="logo">
-		<img src="<?=TEMPLATEURL?>beta/img/loginLogo.png" />
-	</div>	
-	<div id="motto">
-		<img src="<?=TEMPLATEURL?>/v2/static/images/motto.png" />
-	</div>
-	<div id="welcomeSocialBtn"> 
-		<div id="faceicon" onclick="javascript:location.href='/oauth/facebook';"></div>
-		<div id="twittericon" onclick="javascript:location.href='/oauth/twitter';"></div>
-	</div>
-	<div id="form">
-	<form action="" method="post" id="loginF"> 
-		
-		<div style="clear:both;"></div>
-		<input type="text" tabindex="2" name="email" id="loginemail" value="E-Posta:" onFocus="if(this.value=='E-Posta:')this.value='';" onblur="if(this.value=='') this.value='E-Posta:'; " >
-		
-		<input type="text" id="loginpass_show"  tabindex="2" class="focus" value="Parola" rel="loginpass"  name="pw" />
-		<input type="password" id="loginpass" name="password" value="" class="blur" style="display:none;" />
-		
-		<span id="forget_password" class="forget_password">
-			<a href="javascript:;" class="dlink" style="float:right">Şifremi Unuttum</a>
-		</span>
-		<div style="clear:both;"></div> 
-		<input type="submit"  id="loginBtnA"  class="girisBtn" value="Giriş Yap" />
-	</form>
-	</div>	
-	<div id="formLogin"> 
-	<form id="applyform" action="" method="post">
-		<div style="clear:both;"></div>
-		<input type="text" name="name" tabindex="5" value="İsim Soyisim:" onFocus="if(this.value=='İsim Soyisim:')this.value='';" onblur="if(this.value=='') this.value='İsim Soyisim:'; " >
-		<input type="text" name="userName" tabindex="5" value="Kullanıcı Adı:" onFocus="if(this.value=='Kullanıcı Adı:')this.value='';" onblur="if(this.value=='') this.value='Kullanıcı Adı:'; " >
-		<input type="text" name="email" tabindex="6" value="E-Posta:" onFocus="if(this.value=='E-Posta:')this.value='';" onblur="if(this.value=='') this.value='E-Posta:'; " >
-		<input type="text" id="password_show"  tabindex="7" class="focus" value="Parola" rel="password"  name="pw" />
-		<input type="password" tabindex="8" name="password" id="password" value="" class="blur" style="display:none;"/>
-		<input type="text" id="password2_show"  tabindex="9" class="focus" value="Parola Tekrar" rel="password2" name="pw2" />
-		<input type="password" tabindex="10" name="password2" id="password2" value="" class="blur" style="display:none;"/>
-		<select id="selectB" name="male" tabindex="11">  
-			<option value="unknow">Cinsiyet</option>
-			<option value="male">Erkek</option>
-			<option value="female">Kadın</option>
-		</select>
-		<input type="checkbox" name="sozlesmeyiOkudum" id="agree" class="styled" value="1" tabindex="12" />
-		<label for="agree" id="soLabel"> Kullanım <a href="http://democratus.com/about#hizmet" target="_blank">sözleşmesini okudum</a>, kabul ediyorum</label>
-		<input type="button" id="registerBtn" class="kaydolBtn" value="Kayıt Ol" tabindex="13"/>	
-	</form>
-	<div id="message" style="display:none; color:#fff; margin-top:5px;">
-		
-	</div>
-	</div>
-<div style="clear:both;"></div>
-	<div class="footer">
-		<img src="<?=TEMPLATEURL?>/v2/static/images/hr.png" align="center" />
-		<div class="footerContent">
-		<div style="margin-left:35px; float:left;">
-			<a href="https://www.facebook.com/democratustr" target="_blank"><img src="<?=TEMPLATEURL?>/v2/static/images/fbminiicon.png" /></a>
-			<a href="https://twitter.com/#!/democratus_tr" target="_blank"><img src="<?=TEMPLATEURL?>/v2/static/images/twminiicon.png" /></a>
-		</div>
-			<div style="float:right; color:#ccc;">
-				<a href="/about">Democratus</a> | 
-				<a href="/contact">İletişim</a>  
-			</div>
-		</div>
-	</div>
-	
-<?php
-        }
-        public function main1(){
-            global $model, $db;
-            
-            //print_r($_COOKIE);
-            
-            $model->view = 'wellcome';
-            $model->addScript(PLUGINURL . 'lib/en.js', 'en.js', 1 );
-            $model->addScript(PLUGINURL . 'lib/'.$model->language.'.js', $model->language.'.js', 1);
-            $model->addScript(PLUGINURL . 'lib/combined.js', 'combined.js', 1 );
-            $model->addScript(PLUGINURL . 'lib/common.js', 'common.js', 1 );
-            $model->addScript($model->pluginurl . 'wellcome.js', 'wellcome.js', 1 );
-            
-            $model->addStyle(PLUGINURL . 'lib/jquery-ui/jquery-ui.css', 'jquery-ui.css', 1 );
-            require_once(PLUGINPATH.'lib/recaptcha/recaptchalib.php');
-            
-?>
-<!--wellcome-->
-<div id="wellcome">
-  <!--wellcome-head-->
-  <div id="wellcome-head">
-    <div id="wellcome-headleft">Yeni Anket: <strong>Libya Krizinde Türkiye’nin Tavrı Yerinde mi?</strong></div>
-    <div id="wellcome-headright">
-      <input type="text" name="wellcome-search-input" id="wellcome-search-input" value="" />
-      <input type="button" name="gosearch" id="wellcome-gosearch" value="" />
-    </div>
-  </div>
-  <!--wellcome-head END-->
-  <div id="wellcome-spot">
-    <h1>Gündemi Belirle, Düşüncelerini Paylaş</h1>
-  </div>
-  <div id="wellcome-underlogo">
-    <div id="wellcome-underlogo-newfriends">Democratus ile yeni <a href="#">arkadaşlar edinin.</a></div>
-    <div id="wellcome-underlogo-sharemind">Düşüncelerinizi tüm <a href="#">dünya ile paylaşın.</a></div>
-    <div id="wellcome-underlogo-makeagenda">Gündemi siz <a href="#">belirleyin.</a></div>
-  </div>
-  <div id="loginbox">
-      <form action="/user/login/" method="post" id="loginform" onsubmit="" >
-        <input type="text" id="loginemail" value="" class="logininput" />
-        <input type="password" id="loginpass" value="" class="logininput" />
-        <input type="submit" value="GİRİŞ" id="wellcome-login" />
-      </form>
-    <div id="loginbox-forget">&gt;<a href="#">Şifremi Unuttum</a></div>
-  </div>
-  <div id="wellcome-register">
-    <h3 id="wellcome-register-title">Kaydolun, Gündemi Siz Belirleyin. </h3>
-    <div id="wellcome-register-button">Ücretsiz Democratus Hesabı <a href="#">Oluşturun.</a></div>
-    <div id="wellcome-registerbox" style="display: none;">
-      <form action="/user/new/" method="post" class="form" id="applyform">
-        <span class="message">all fields are required</span>
-        <p>
-          <label>E-mail</label>
-          <input type="text" id="email" name="email" value="" autocomplete="off" />
-          
-        </p>
-        
-        <p>
-          <label>Password</label>
-          <input type="password" id="password" name="password" value="" class="small" autocomplete="off" />
-          
-        </p>        
-        
-        <p>
-          <label>Password Again</label>
-          <input type="password" id="password2" name="password2" value="" class="small" autocomplete="off" />
-          
-        </p>
-              
-      
-        <p><label>Name</label>
-        <input type="text" name="name" value="" autocomplete="off" />
-        </p>
-
-        <p><label>Motto</label>
-        <textarea cols="5" rows="5" name="motto" id="motto"></textarea>
-        </p>
-
-        <p><label>Country</label>
-        <?php echo $model->country_to_select('country', 146)?>
-        
-        </p>
-
-        <p><label>City</label>
-        <?php echo $model->city_to_select('city', 146, 9928)?>
-        
-        </p>
-
-        <p>
-            <?php echo recaptcha_get_html("6LdXIQsAAAAAANstV0tV1XiVkrrCMZTegsaIJsRz"); ?>
-        </p>
-        
-        <p id="wellcomeagree">
-          <label>&nbsp;</label>
-          <input type="checkbox" value="1" name="agree" id="agree" />Üyelik kurallarını okumuş gibi yaptım.
-        
-        </p>
-        
-        
-        <p>
-          <label>&nbsp;</label>
-          <input type="submit" value="Kaydol" id="applybutton" disabled="disabled" />
-        </p>
-      </form>
-    </div>
-  </div>
-</div>
-<!--wellcome END-->
-<?php
-        }
     }
 ?>
--- t/ala/welcome.php
+++ t/ala/welcome.php
@@ -2,81 +2,38 @@
         <meta charset="utf-8">
         <meta content="IE=edge,chrome=1" http-equiv="X-UA-Compatible">
         
-		<!-- SEO Meta -->
-		<title>Democratus :: Giriş</title>
-		<meta content="Bir başka sosyal paylaşım platformu..." name="description">
-		<meta content="noindex, nofollow" name="robots">
-		<meta content="noindex, nofollow" name="googlebot"><!-- // SEO Meta -->
+        <title>Democratus - Dünyayı Fikirlerinle Şekillendir</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="Beaker - Responsive Landing Page">
+        <meta name="author" content="Shy Markets">
+        <meta content="Bir başka sosyal paylaşım platformu..." name="description">
+        <meta content="noindex, nofollow" name="robots">
+        <meta content="noindex, nofollow" name="googlebot"><!-- // SEO Meta -->
         
-		<meta content="width=device-width" name="viewport">
-		
-		
-		<?php 
-			global $model;
-			$model->addStyle('http://fonts.googleapis.com/css?family=Ubuntu:700&subset=latin,latin-ext', 'http://fonts.googleapis.com/css?family=Ubuntu:700&subset=latin,latin-ext', 1 ); 
-			$model->addStyle($this->url.'css/bootstrap-custom-build.css', 'bootstrap-custom-build.css', 1 ); 
-			$model->addStyle($this->url.'css/bootstrap-helper.css', 'bootstrap-helper.css', 1 ); 
-			$model->addStyle($this->url.'css/blitzer/jquery-ui-1.9.2.custom.min.css', 'blitzer/jquery-ui-1.9.2.custom.min.css', 1 ); 
-			$model->addStyle($this->url.'css/welcome.css', 'app.css', 1 ); 
-			echo  '<!--[if lte IE 9]>';
-			$model->addStyle($this->url.'css/ie.css', 'ie.css', 1 ); 
-			echo '<![endif]-->';
-			
-	        
-		?>
-		<?php 
-	       
-		?>
+        <!-- Le styles -->
+        <?php 
+            global $model;
+            $model->addStyle($this->url.'css/bootstrap-combined.no-icons.min.css', 'bootstrap-combined.no-icons.min.css', 1 ); 
+            $model->addStyle($this->url.'css/custom.css', 'custom.css', 1 );
+            $model->addStyle($this->url.'css/font-awesome.css', 'font-awesome.css', 1 );
+            $model->addStyle($this->url.'js/fancybox/jquery.fancybox.css', 'js/fancybox/jquery.fancybox.css', 1 );
+            
 
-		
-		
-		<!-- Google Web Font -->
-		
-	 	<!--[if lte IE 9]><link rel="stylesheet" href="css/ie.css"><![endif]-->
-		<link href="favicon.ico" rel="shortcut icon">
-		<!-- Bu web sitesinin tasarımı ve kodlaması özgündür! -->
-		<meta content="Atölye15" name="generator">
+            $model->addStyle($this->url.'css/font-awesome-ie7.css', 'font-awesome-ie7.css', 1,"<!--[if IE 7]>" );
+
+            $model->addScript($this->url.'js/html5shiv.min.js', 'html5shiv.min.js', 1,"<!--[if lt IE 9]>" );
+
+            $model->addStyle($this->url.'css/ie.css', 'ie.css', 1,"<!--[if lt IE 9]>" );
+
+        ?>
+
     </head>
-    <body class="atolye15_body login">
+    <body>
     {{main}} 
-    		<h1 class="logo"><a href="#">Democratus</a><span>Dünyayı fikirlerinle şekillendir!</span></h1>
-    		
-    		<div class="clearfix"></div>
-    		
-    		{{login}}
-    		
-    		<div class="clearfix"></div>
-    		<? /*
-    		<ul class="features">
-    			<li>
-    				<div class="img"><img alt="" src="<?=TEMPLATEURL?>/ala/img/feature-1.jpg"></div>
-    				<h3>Fikrini paylaş,<br>sesini tüm ülke duysun!</h3>
-    			</li>
-    			<li>
-    				<div class="img"><img alt="" src="<?=TEMPLATEURL?>/ala/img/feature-2.jpg"></div>
-    				<h3>Referandumlara oy ver,<br>ülke gündemini değiştir!</h3>
-    			</li>
-    			<li>
-    				<div class="img"><img alt="" src="<?=TEMPLATEURL?>/ala/img/feature-3.jpg"></div>
-    				<h3>#konu ve #kurum sayfalarıyla,<br>yönetimlere yardım et!</h3>
-    			</li>
-    			<li class="last">
-    				<div class="img"><img alt="" src="<?=TEMPLATEURL?>/ala/img/feature-4.jpg"></div>
-    				<h3>Vekil seçilerek,<br>gündemleri sen belirle!</h3>
-    			</li>
-    		</ul>
-    		*/ ?>
-    		<div class="clearfix"></div>
-    		
-    		<section class="signup" style="display:none;">
-    			<button id="wellcome-register-button" class="btn btn-large btn-block">Hemen Kayıt Ol</button>
-    			<div class="clearfix"></div>
-    		</section>
-    		
-    		<div class="clearfix"></div>
-    
-       <!-- <script>window.jQuery || document.write('&lt;script src="js/jquery-1.8.2.min.js"&gt;&lt;\/script&gt;')</script><script src="js/jquery-1.8.2.min.js"></script>
-				<script src="js/login.js"></script>
-    -->
+
+    <?php
+        
+    ?>
+
     {{googleanalytics}}
 </body></html>
